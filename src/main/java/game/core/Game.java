49fcbd8ef5b25320bde0a0a1fd45a7bef05691b2
alina <tabaluga@debian-BULLSEYE-live-builder-AMD64>
2023-03-27 14:26:11 +0200

    Added possible methods to IGame and added Game class

---
+++ src/main/java/game/core/Game.java
@@ -0,0 +1,28 @@
+package game.core;
+
+
+public class Game implements IGame{
+    @Override
+    public void showStartScreen() {
+        // TODO implement
+    }
+
+    @Override
+    public void establishNetworkConnection() {
+        // TODO implement
+    }
+
+    @Override
+    public void initializeGameUI() {
+        // TODO implement
+    }
+
+    @Override
+    public void initializeGameLogic() {
+        // TODO implement
+    }
+
+    public static void main(String[] args){
+        System.out.println("Please call the other methods here when implemented");
+    }
+}
--- src/main/java/game/core/IGame.java
+++ src/main/java/game/core/IGame.java
@@ -1,4 +1,29 @@
 package game.core;
 
+/***
+ * This is the games starting point (Main)
+ * IGame puts all the parts of the game together
+ */
 public interface IGame {
+    /**
+     * showStartScreen - shows the start screen with the options needed to start a new game
+     */
+    public void showStartScreen();
+
+    /**
+     * establishNetworkConnection - handles the initialization of the network connection and gives back an object to
+     * manage the network connection
+     */
+    public void establishNetworkConnection();
+
+    /**
+     * initializeGameUI - initializes the UI and gives back an object to manage the UI
+     */
+    public void initializeGameUI();
+
+    /**
+     * initializeGameLogic - prepares the model objects for the characters (mice, cats), the game field and prepares the
+     * scoring
+     */
+    public void initializeGameLogic();
 }
