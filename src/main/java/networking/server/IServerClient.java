8b5d85733d80473281c0303219d700830ff25a55
Alex Hold <alex@hold.re>
2023-03-28 17:47:42 +0200

    added javadoc for IServerClient

--- src/main/java/networking/server/IServerClient.java
+++ src/main/java/networking/server/IServerClient.java
@@ -8,17 +8,45 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public interface IServerClient {
 
+    /**
+     * dispatches the server client to a new thread (listen to new messages thread)
+     */
     void dispatch();
 
+    /**
+     * returns the thread in which the serverClient is currently running in
+     * @return the thread of the serverClient
+     */
     Thread getThread();
 
+    /**
+     * stops the serverClient and closes it socket
+     */
     void stop();
 
+    /**
+     * returns the received messages of the serverClient. When this function is called it
+     * will delete the messages afterwards. So it is more like a get and clear method.
+     * @return the received messages since the last getMessage() call
+     */
     List<IMessage> getMessages();
 
+    /**
+     * sends the given message to the connected client
+     * @param message the message to send
+     * @throws IOException
+     */
     void sendMessage(IMessage message) throws IOException;
 
+    /**
+     * returns the address (IP) of the connected client
+     * @return ip of the connected client
+     */
     String getAddress();
 
+    /**
+     * returns whether the client is disconnected or not 
+     * @return whether the client is disconnected or not
+     */
     boolean isDisconnected();
 }
