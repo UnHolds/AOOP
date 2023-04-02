374c73f609a1a2e7ab9338b3e37fa031271958a6
alina <tabaluga@debian-BULLSEYE-live-builder-AMD64>
2023-04-02 18:15:08 +0200

    GameLogic: Added IPlayer methods and implemented Player

---
+++ src/main/java/game/core/models/Player.java
@@ -0,0 +1,28 @@
+package game.core.models;
+
+public class Player implements IPlayer {
+    private int score;
+    private int gameUIId;
+    private String name;
+
+    public Player(int gameUIId, String name){
+        this.score = 0;
+        this.gameUIId = gameUIId;
+        this.name = name;
+    }
+
+    @Override
+    public int getScore() {
+        return this.score;
+    }
+
+    @Override
+    public int getGameUIId() {
+        return this.gameUIId;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+}
