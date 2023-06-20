1f59a0366507d7a9a451b5dcf18a40b2bee959d0
Alex Hold <alex@hold.re>
2023-06-20 13:44:35 +0200

    added the creation of subways

--- src/main/java/game/core/GameInit.java
+++ src/main/java/game/core/GameInit.java
@@ -1,11 +1,13 @@
 package game.core;
 
-import game.core.models.IMouse;
-import game.core.models.IPlayer;
-import game.core.models.ISubway;
+import game.core.models.*;
+import game.core.models.impl.Exit;
+import game.core.models.impl.Position;
+import game.core.models.impl.Subway;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class GameInit {
 
@@ -17,9 +19,53 @@ public class GameInit {
     private int rowCount = 18;
     private int colCount = 25;
 
+    private int numberOfSubways = 2; //needs to be at least 2
+
+    private int numberOfMice = 5;
+
+
     public GameInit() {
-        subways = new ArrayList<>();
-        mice = new ArrayList<>();
+        generateSubways();
+        generateMice();
+    }
+
+    private IPosition getRandomPositionInGameField(){
+        Random rand = new Random();
+        int x = rand.nextInt(colCount);
+        int y = rand.nextInt(rowCount);
+
+        return new Position(x,y);
+    }
+
+    private void generateSubways(){
+        Random rand = new Random();
+        this.subways = new ArrayList<>();
+
+        for(int i = 0; i < numberOfSubways; i++){
+
+            int numberOfExits = 2 + rand.nextInt(3);
+            List<IExit> exits = new ArrayList<>();
+
+            for(int j = 0; j < numberOfExits; j++){
+                exits.add(new Exit(getRandomPositionInGameField()));
+            }
+
+            Subway subway = new Subway(exits);
+
+            if(i == 0){
+                subway.setGoal(true);
+            }else{
+                subway.setGoal(false);
+            }
+
+            this.subways.add(subway);
+        }
+    }
+
+
+    private void generateMice(){
+        this.mice = new ArrayList<>();
+        //TODO create mice
     }
 
 
